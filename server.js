cff0973bbcfa0b42eb724fed7cc6e2a5eed5b99c
Nate A <59675817+nargaw@users.noreply.github.com>
2021-12-30 20:27:56 -0500

    serve an HTML file

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ if (!process.env.DISABLE_XORIGIN) {
 // })
 
 app.get("/", function(req, res){
-    res.send("Hello Express")
+    res.sendFile(__dirname + "/views/index.html")
 })
 
 //Listen on port set in environment variable or default to 3000
