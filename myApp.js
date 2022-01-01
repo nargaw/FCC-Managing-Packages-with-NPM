409ad618ab5b9ed497a85c9c05416d2df64854e8
Nate A <59675817+nargaw@users.noreply.github.com>
2021-12-31 21:08:45 -0500

    change to arrow functions

--- myApp.js
+++ myApp.js
@@ -6,13 +6,13 @@ var bGround = require('fcc-express-bground');
 bGround.log("Hello World")
 console.log("Hello World")
 
-app.get("/", function(req, res){
+app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/views/index.html")
 })
 
 app.use('/public', express.static(__dirname + "/public"))
 
-app.get('/json', function(req, res){
+app.get('/json', (req, res) =>{
     res.json(
         {"message": "Hello json"}
     )
