2c5f68f9d75ab0307c5e75e94ffa145b9ae39782
Nate A <59675817+nargaw@users.noreply.github.com>
2022-01-01 20:03:27 -0500

    add time logger middleware

--- myApp.js
+++ myApp.js
@@ -33,7 +33,13 @@ app.get('/json', (req, res) => {
 })
 
 
-
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+}, (req, res) => {
+    res.send({"time": req.time})
+    console.log({"time": req.time})
+})
 
 
 
