094cba789cd25b79526c3f423e10277da7f5935d
Nate A <59675817+nargaw@users.noreply.github.com>
2022-01-02 19:36:54 -0500

    get data from POST requests

--- myApp.js
+++ myApp.js
@@ -54,6 +54,10 @@ app.get("/name", (req, res) =>{
     res.json({ name: req.query.first + " " + req.query.last})
 })
 
+app.post("/name", (req, res) => {
+    res.json({ name: req.body.first + " " + req.body.last})
+})
+
 
 
 
